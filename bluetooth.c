1e722612c4c47c02b4823750e0e9d5af8e74e509
Arjan van der Ven <arjan@linux.intel.com>
2007-05-22 18:34:37 +0000

    suggest removing hci_usb
    git-svn-id: https://powertop.googlecode.com/svn/trunk@54 0bbd23ad-e62f-0410-b3b3-4ddab988b7fa

--- bluetooth.c
+++ bluetooth.c
@@ -116,7 +116,7 @@ void suggest_bluetooth_off(void)
 
 	if (thisbytes == previous_bytes) {
 		printf( "Suggestion: Disable the unused bluetooth interface with the following command:\n"
-			"  hciconfig hci0 down \n"
+			"  hciconfig hci0 down ; rmmod hci_usb\n"
 			"Bluetooth is a radio and consumes quite some power, and keeps USB busy as well.\n");
 		suggestioncount++;
 	}
