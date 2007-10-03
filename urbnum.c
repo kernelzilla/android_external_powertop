442fddf7735534c91a6418a988f75d634a6d4768
Arjan van der Ven <arjan@linux.intel.com>
2007-10-03 20:17:02 +0000

    prettify output
    git-svn-id: https://powertop.googlecode.com/svn/trunk@257 0bbd23ad-e62f-0410-b3b3-4ddab988b7fa

--- urbnum.c
+++ urbnum.c
@@ -61,7 +61,7 @@ static void update_urbnum(char *path, uint64_t count)
 	struct device_data *ptr;
 	FILE *file;
 	char fullpath[4096];
-	char name[4096];
+	char name[4096], vendor[4096];
 	ptr = devices;
 	while (ptr) {
 		if (strcmp(ptr->pathname, path)==0) {
@@ -80,15 +80,28 @@ static void update_urbnum(char *path, uint64_t count)
 	ptr->urbs = ptr->previous_urbs = count;
 	sprintf(fullpath, "%s/product", path);
 	file = fopen(fullpath, "r");
+	memset(name, 0, 4096);
 	if (file) { 
 		fgets(name, 4096, file);
-		/* some devices have bogus names */
-		if (strlen(name)<4)
-			strcpy(ptr->human_name, path);
-		else
-			sprintf(ptr->human_name, "USB device: %s", name);
 		fclose(file);
 	}
+	sprintf(fullpath, "%s/manufacturer", path);
+	file = fopen(fullpath, "r");
+	memset(vendor, 0, 4096);
+	if (file) { 
+		fgets(vendor, 4096, file);
+		fclose(file);
+	}
+	
+	if (strlen(name)>0 && name[strlen(name)-1]=='\n')
+		name[strlen(name)-1]=0;
+	if (strlen(vendor)>0 && vendor[strlen(vendor)-1]=='\n')
+		vendor[strlen(vendor)-1]=0;
+	/* some devices have bogus names */
+	if (strlen(name)<4)
+		strcpy(ptr->human_name, path);
+	else
+		sprintf(ptr->human_name, "     USB device : %s (%s)", name, vendor);
 	
 }
 
