869de2ce7f7208218044aff64fe453234c20627c
Auke Kok <auke-jan.h.kok@intel.com>
2008-11-26 18:58:21 +0000

    fix-suggest-usb-autosuspend.dpatch by Patrick Winnertz <winnie@debian.org>
    changes /dev/scd0 device node reference to /dev/cdrom.
    
    
    git-svn-id: https://powertop.googlecode.com/svn/trunk@301 0bbd23ad-e62f-0410-b3b3-4ddab988b7fa

--- powertop.c
+++ powertop.c
@@ -1101,7 +1101,7 @@ int main(int argc, char **argv)
 		/* suggest to stop hal polilng if it shows up in the top 50 and wakes up too much*/
 		suggest_process_death("hald-addon-stor : ", "hald-addon-storage", lines, min(linehead,50), 2.0,
 				    _( "Suggestion: Disable 'hal' from polling your cdrom with:  \n" 
-				       "hal-disable-polling --device /dev/scd0 'hal' is the component that auto-opens a\n"
+				       "hal-disable-polling --device /dev/cdrom 'hal' is the component that auto-opens a\n"
 				       "window if you plug in a CD but disables SATA power saving from kicking in."), 30);
 
 		/* suggest to kill sealert; it wakes up 10 times/second on a default F7 install*/
